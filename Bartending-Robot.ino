06a2b4027da3c939679158e2ab8544d792013f4e
mercurywaters <github@mercurywaters.com>
2015-05-04 20:49:17 -0700

    Queue library added
    Custom Queue Library Added

--- Bartending-Robot.ino
+++ Bartending-Robot.ino
@@ -1,4 +1,4 @@
-
+#include <BarvizQueue.h>
 
 // comment the following define statement to shut the built-in LED off
 #define USE_DEBUG_LED 1
@@ -11,10 +11,20 @@
 #define ORDER_QUEUE_SIZE  4
 
 const int buttonPins [ NUMBER_OF_BUTTONS ] = { 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
-const int orderQueue [ ORDER_QUEUE_SIZE ] = { -1 };
+
+typedef struct {
+  int orderIndex;
+  
+  String toString ();
+} Order;
+
+String Order :: toString () {
+  return " Testing";
+}
+
+BarvizQueue <Order> orderQue ( ORDER_QUEUE_SIZE );
 
 void setup() {
-  // put your setup code here, to run once:
   
   int i = 0;
   for ( i = 0; i < NUMBER_OF_BUTTONS; i ++ ) {
@@ -30,53 +40,49 @@ void setup() {
 #if defined( SOFTWARE_DEBUG )
   Serial.begin(9600);
   delay(2000);// Give reader a chance to see the output.
+  orderQue.setPrinter ( Serial );
 #endif
 
 }
 
-#define BUTTON_SENSITIVITY_MILLIS  3
+#define BUTTON_SENSITIVITY_MILLIS  250
 
 elapsedMillis millisSinceInterrupt = 0;
-volatile boolean inInterrupt = false;
-volatile int menuID = -1;
 
 void pinChanged () {
   
-  if ( !inInterrupt ) {
-    
-    inInterrupt = true;
-    
-    if ( millisSinceInterrupt > BUTTON_SENSITIVITY_MILLIS ) {
-  
-      for ( int i = 0; i < NUMBER_OF_BUTTONS; i ++ ) {
-        
-        boolean value = ( digitalRead ( buttonPins [ i ] ) == LOW );
-        
-        if ( value == true ) {
-          menuID = i;
+  if ( millisSinceInterrupt > BUTTON_SENSITIVITY_MILLIS ) {
+
+    for ( int i = 0; i < NUMBER_OF_BUTTONS; i ++ ) {
+      
+      boolean value = ( digitalRead ( buttonPins [ i ] ) == LOW );
+      
+      if ( value == true ) {
+        if ( !orderQue.isFull () ) {
+          Order * order = new Order ();
+          order -> orderIndex = i;
+          orderQue.add ( order );
         }
       }
     }
-
-    inInterrupt = false;
-    millisSinceInterrupt = 0;
   }
-}
 
-int previousMenuID = -1;
+  millisSinceInterrupt = 0;
+}
 
 #if defined( USE_DEBUG_LED )
 bool builtInLedState = HIGH;
 #endif
 
+int previousQueueSize = 0;
+
 void loop() {
 
-  if ( previousMenuID != menuID ) {
-    Serial.print ( "Selected Menu: " );
-    Serial.println ( menuID );
+  if ( previousQueueSize != orderQue.size () ) {
+    orderQue.print ();
   }
   
-  previousMenuID = menuID;
+  previousQueueSize = orderQue.size();
 
 #if defined( USE_DEBUG_LED )
   builtInLedState = !builtInLedState;
---
+++ libraries/BarvizQueue/BarvizQueue.h
@@ -0,0 +1,183 @@
+#ifndef _BARVIZ_QUEUE_H
+#define _BARVIZ_QUEUE_H
+
+// include Arduino basic header.
+#include <Arduino.h>
+
+template <typename T> class BarvizQueue {
+
+  private:
+    unsigned int capacity;
+    int currSize;
+  
+  public:
+    BarvizQueue ( const unsigned int );
+    ~BarvizQueue ();
+    
+    bool add ( T * );
+    T * remove ();
+    T * peek () const;
+    T * removeMostRecent ();
+    
+    bool isEmpty () const;
+    bool isFull () const;
+    int size () const;
+    
+    void setPrinter (Print & p);
+    void print ();
+
+  private:
+    typedef struct Node {
+      T * item;
+      Node * next;
+      Node * prev;
+    } Node;
+    
+    Node * head;
+    Node * tail;
+
+    Print * printer;
+};
+
+template <typename T> BarvizQueue<T> :: BarvizQueue ( const unsigned int maxCapacity) {
+  this -> capacity = maxCapacity;
+  this -> currSize = 0;
+  this -> head = NULL;
+  this -> tail = NULL;
+}
+
+template <typename T> BarvizQueue<T> :: ~BarvizQueue () {
+
+  for (Node * ptr = this -> head; ptr != NULL; ptr = ptr -> next) {
+    delete ptr;
+  }
+
+  this -> currSize = 0;
+  this -> head = NULL;
+  this -> tail = NULL;
+}
+
+template <typename T> bool BarvizQueue<T> :: add ( T * element ) {
+  
+  bool returnValue = false;
+
+  if ( currSize < capacity ) {  
+    
+  	Node * ptr = new Node ();
+  
+  	ptr -> item = element;
+  	ptr -> next = NULL;
+  	ptr -> prev = NULL;
+    
+    if ( this -> head == NULL ) { // tail should be NULL too, in this case
+      this -> head = this -> tail = ptr;
+    }
+    else {
+      ptr -> prev = tail;
+      this -> tail -> next = ptr;
+      this -> tail = ptr;
+    }
+    
+    this -> currSize ++;
+    
+    returnValue = true;
+  }
+  
+  return returnValue;
+}
+
+template <typename T> bool BarvizQueue<T> :: isEmpty () const {
+  return ( this -> currSize == 0 );
+}
+
+template <typename T> bool BarvizQueue<T> :: isFull () const {
+  return ( this -> currSize == capacity );
+}
+
+template <typename T> int BarvizQueue<T> :: size () const {
+  return this -> currSize;
+}
+
+template <typename T> T * BarvizQueue<T> :: remove () {
+
+  T * returnValue = NULL;
+  
+  if (!isEmpty ()) {
+
+    returnValue = this -> head -> item;
+    
+    Node * ptr = this -> head;
+
+    this -> head = this -> head -> next;
+    delete ptr;
+    
+    if ( this -> head != NULL ) {
+      this -> head -> prev = NULL;
+    }
+    else { // make tail NULL as well
+      this -> tail = NULL;
+    }
+    
+    this -> currSize --;
+  }
+  
+  return returnValue;
+}
+
+template <typename T> T * BarvizQueue<T> :: peek () const {
+
+  T * returnValue = NULL;
+
+  if ( !isEmpty () ) {
+    returnValue = this -> head -> item;
+  }
+  
+  return returnValue;
+}
+
+template <typename T> T * BarvizQueue<T> :: removeMostRecent () {
+
+  T * returnValue = NULL;
+  
+  if (!isEmpty ()) {
+
+    returnValue = this -> tail -> item;
+    
+    Node * ptr = this -> tail;
+
+    this -> tail = this -> tail -> next;
+    delete ptr;
+    
+    if ( this -> tail != NULL ) {
+      this -> tail -> next = NULL;
+    }
+    else { // make head NULL as well
+      this -> head = NULL;
+    }
+    
+    this -> currSize --;
+  }
+  
+  return returnValue;
+}
+
+template <typename T> void BarvizQueue<T> :: setPrinter ( Print & p ) {
+  this -> printer = &p;
+}
+
+template <typename T> void BarvizQueue<T> :: print () {
+  if ( printer != NULL ) {
+    printer -> print ( "[ " );
+    for ( Node * ptr = this -> head; ptr != NULL; ptr = ptr -> next ) {
+      printer -> print ( ptr -> item -> toString () );
+      if ( ptr != this -> tail ) {
+        printer -> print ( ", " );
+      }
+    }
+    printer -> println ( " ]" );
+  }
+}
+
+
+#endif // _BARVIZ_QUEUE_H
+
